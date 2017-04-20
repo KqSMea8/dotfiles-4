6e4b6575902b5911475c305da18adbed6a8b9068
Jiajun Liu <iamljj@gmail.com>
2017-04-20 12:32:38 +0800

    auto install vim manage plugin

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ ln -f tmux.conf ~/.tmux.conf
 ln -f gitconfig ~/.gitconfig
 ln -f sshconfig ~/.ssh/config
 
+mkdir ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
 echo "Done!"
